8ca1074f4845dd2d2962ee7db588f46fb7a89061
eehwan <eehwan.shin@gmail.com>
2020-12-30 21:40:26 +0900

    first commit

---
+++ Script/BgScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgScroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    
+    //}
+    public float scrollSpeed = 1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z = Mathf.Repeat(-Time.time * scrollSpeed, 40.0f);
+        transform.position = newPosition;
+    }
+}
